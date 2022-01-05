e4ada108c2adea5cd85dd3d3960a40f8846a876e
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2022-01-05 14:18:53 -0600

    data acquire, clean, and prep complete!

--- wrangle_jones.py
+++ wrangle_jones.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 ##########################################################################################| ACQUIRE FUNCTION
 def acquire_zillow():
@@ -30,8 +30,8 @@ THE CONNECTION STRING.
     else: 
         db = 'zillow'
         query  = '''
-            SELECT bathroomcnt as baths, bedroomcnt as beds, calculatedfinishedsquarefeet as sqft, fips, fullbathcnt as fullbaths, latitude,
-                   longitude, roomcnt as rooms, yearbuilt, taxvaluedollarcnt as tax_value, garagecarcnt, logerror, transactiondate, 
+            SELECT bathroomcnt as baths, bedroomcnt as beds, calculatedfinishedsquarefeet as sqft, fullbathcnt as fullbaths, latitude,
+                   longitude, yearbuilt, taxvaluedollarcnt as tax_value, logerror, transactiondate, 
                    unitcnt, propertylandusetypeid
             FROM properties_2017
             LEFT JOIN predictions_2017 pred USING(parcelid)
@@ -61,7 +61,6 @@ def wrangle_zillow(df):
 THIS FUNCTION TAKES IN A RAW DATAFRAME AND PERFORMS THE FOLLOWING DATA CLEANING:
     1) FILTER OUT NON-SINGLE UNIT OBSERVATIONS
     2) DROPS NULLS
-    3) ADDS COLUMNS WITH LABELED COUNTIES BASED ON FIPS CODES
     4) CORRECTING DTYPES
     5) CREATE AGE COLUMN
     6) DROP COLUMNS
@@ -71,29 +70,82 @@ THIS FUNCTION TAKES IN A RAW DATAFRAME AND PERFORMS THE FOLLOWING DATA CLEANING:
     #filter single units by properylandusetype, bath, bed, and sqft count, and unit count
     df = df[df.propertylandusetypeid.isin([261, 262, 263, 264, 266, 268, 273, 276, 279])]
     df = df[(df.baths > 0) & (df.beds > 0) & (df.sqft > 300)]
-    df = df[df.unitcnt == 1]
     
     #dropping null rows and columns with > 50% of values missing
-#    df = df.dropna(axis = 1, thresh = .5 * len(df))
-#    df = df.dropna(thresh = .5 * len(df.columns))
-    df.dropna(inplace = True)
+    df = df.dropna(axis = 1, thresh = .5 * len(df))
+    df = df.dropna(thresh = .5 * len(df.columns))
     
-    #label fips counties
-    df['fips'] = df.fips.astype(int)
-    df['fips_loc'] = df.fips.replace({6037:'Los Angeles, CA', 6059:'Orange, CA', 6111:'Ventura, CA'})
-    
-    #correcting dtypes
-    df[['beds', 'sqft', 'fullbaths', 'latitude', 'longitude', 'rooms', 'yearbuilt', 'unitcnt']] = df[['beds', 'sqft',\
-                                                                                                    'fullbaths', 'latitude', 'longitude', 'rooms', 'yearbuilt', 'unitcnt']].astype('int')
+#    #label fips counties
+#    df['fips'] = df.fips.astype(int)
+#    df['fips_loc'] = df.fips.replace({6037:'Los Angeles, CA', 6059:'Orange, CA', 6111:'Ventura, CA'})
     
     #create age column from yearbuilt
     df['age'] = 2022 - df.yearbuilt
     
+    #drop any remaining rows with null values
+    df.dropna(inplace = True)
+    
+    #correcting dtypes
+    df[['beds', 'sqft', 'fullbaths', 'latitude', 'longitude', 'yearbuilt', 'unitcnt']] = df[['beds', 'sqft',\
+                                                                                                    'fullbaths', 'latitude', 'longitude', 'yearbuilt', 'unitcnt']].astype('int')
+    
     #drop columns 
-    df = df.drop(columns = ['propertylandusetypeid', 'fips', 'transactiondate', 'yearbuilt'])
+    df = df.drop(columns = ['propertylandusetypeid', 'transactiondate', 'yearbuilt', 'unitcnt'])
+    
+    #remove outliers
+    for col in df[['baths', 'beds', 'sqft', 'fullbaths', 'tax_value', 'logerror']]:
+        
+        if df[col].dtype != 'O':
+            # get quartiles
+            q1, q3 = df[col].quantile([.25, .75])
+
+            # compute iqr
+            iqr = q3 - q1
+
+            # get cutoff points for removing outliers
+            upper = q3 + 1.5 * iqr
+            lower = q1 - 1.5 * iqr
+
+            # remove outliers
+            df = df[(df[col]>lower)&(df[col]<upper)]
     
     
     return df
     
 
 ##########################################################################################| PREPARE FUNCTION
+#SPLIT
+#SCALE
+def split_data(df):
+    '''
+THIS FUNCTION TAKES IN A CLEAN DF AND SPLITS IT, RETURNING TRAIN, VALIDATE, AND TEST DFs
+    '''
+    
+    train, test = train_test_split(df, train_size = 0.8, random_state = 123)
+    train, validate = train_test_split(train, train_size = 0.75, random_state = 123)
+    
+    return train, validate, test
+
+def scale_data(train, validate, test):
+    '''
+    
+    '''
+    
+    cols_to_scale = ['baths', 'beds', 'sqft', 'fullbaths', 'tax_value', 'logerror', 'age']
+    
+    scaler = StandardScaler()
+    scaler.fit(train[cols_to_scale])
+    
+    train[[f'{col}_scaled' for col in cols_to_scale]] = scaler.transform(train[cols_to_scale])
+    validate[[f'{col}_scaled' for col in cols_to_scale]] = scaler.transform(validate[cols_to_scale])
+    test[[f'{col}_scaled' for col in cols_to_scale]] = scaler.transform(test[cols_to_scale])
+    
+    train_scaled = train
+    validate_scaled = validate
+    test_scaled = test
+    
+    return train_scaled, validate_scaled, test_scaled
+
+    
+    
+
