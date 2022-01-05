5990982d089529ba5a7e58019e21ec4a67ddd158
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2022-01-05 09:55:38 -0600

    wrangle.py initial commit

---
+++ wrangle_jones.py
@@ -0,0 +1,22 @@
+##########################################################################################| WRANGLE.PY
+
+##########################################################################################| IMPORTS
+import os
+
+import env
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+##########################################################################################| ACQUIRE FUNCTION
+
+
+##########################################################################################| WRANGLE FUNCTION
+
+##########################################################################################| PREPARE FUNCTION
