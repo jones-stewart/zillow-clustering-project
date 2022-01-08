7f01563d46580cd01a9d07c27206c85121dbbf25
Sophia Stewart <sophia.marcell.stewart@gmail.com>
2022-01-08 14:41:36 -0600

    Added hard-coded function for use in report

--- model_stewart.py
+++ model_stewart.py
@@ -61,9 +61,9 @@ original unencoded cluster column.
     '''
     
     #encode cluster column
-    dummy_train_df = pd.get_dummies(X_train.cluster, prefix='cluster_')
-    dummy_validate_df = pd.get_dummies(X_validate.cluster, prefix='cluster_')
-    dummy_test_df = pd.get_dummies(X_test.cluster, prefix='cluster_')
+    dummy_train_df = pd.get_dummies(X_train.cluster, prefix='cluster')
+    dummy_validate_df = pd.get_dummies(X_validate.cluster, prefix='cluster')
+    dummy_test_df = pd.get_dummies(X_test.cluster, prefix='cluster')
     
     encoded_train = pd.concat([X_train , dummy_train_df], axis = 1)
     encoded_validate = pd.concat([X_validate , dummy_validate_df], axis = 1)
@@ -75,7 +75,7 @@ original unencoded cluster column.
     
     return X_train_encoded, X_validate_encoded, X_test_encoded
 
-def model_features_df(target, X_scaled_train, X_scaled_validate, X_scaled_test, y_train, y_validate, y_test, model_vars):
+def model_features_df(target, X_train, X_validate, y_train, y_validate, model_features):
 #baseline_prediction
     median = y_train[target].median()
     y_train['baseline_pred'] = median
@@ -86,10 +86,10 @@ def model_features_df(target, X_scaled_train, X_scaled_validate, X_scaled_test, 
 
    #model01_prediction
     lm = LinearRegression(normalize=True)
-    lm.fit(X_scaled_train, y_train[target])
+    lm.fit(X_train, y_train[target])
    
-    y_train['model01_pred'] = lm.predict(X_scaled_train)
-    y_validate['model01_pred'] = lm.predict(X_scaled_validate)
+    y_train['model01_pred'] = lm.predict(X_train)
+    y_validate['model01_pred'] = lm.predict(X_validate)
    
     rsme_model01_train = (mean_squared_error(y_train.logerror, y_train['model01_pred']))**(1/2)
     rsme_model01_validate = (mean_squared_error(y_validate.logerror, y_validate['model01_pred']))**(1/2)
@@ -97,33 +97,89 @@ def model_features_df(target, X_scaled_train, X_scaled_validate, X_scaled_test, 
    
    #model02_prediction
     lars = LassoLars(alpha=1.0)
-    lars.fit(X_scaled_train, y_train[target])
+    lars.fit(X_train, y_train[target])
    
-    y_train['model02_pred'] = lars.predict(X_scaled_train)
-    y_validate['model02_pred'] = lars.predict(X_scaled_validate)
+    y_train['model02_pred'] = lars.predict(X_train)
+    y_validate['model02_pred'] = lars.predict(X_validate)
    
     rsme_model02_train = (mean_squared_error(y_train.logerror, y_train['model02_pred']))**(1/2)
     rsme_model02_validate = (mean_squared_error(y_validate.logerror, y_validate['model02_pred']))**(1/2)
 
     
     #model03_prediction
-    lm = LinearRegression(normalize=True)
-    lm.fit(X_scaled_train[model_vars], y_train[target])
+    lm2 = LinearRegression(normalize=True)
+    lm2.fit(X_train[model_features], y_train[target])
    
-    y_train['model03_pred'] = lm.predict(X_scaled_train[model_vars])
-    y_validate['model03_pred'] = lm.predict(X_scaled_validate[model_vars])
+    y_train['model03_pred'] = lm2.predict(X_train[model_features])
+    y_validate['model03_pred'] = lm2.predict(X_validate[model_features])
    
     rsme_model03_train = (mean_squared_error(y_train.logerror, y_train['model03_pred']))**(1/2)
     rsme_model03_validate = (mean_squared_error(y_validate.logerror, y_validate['model03_pred']))**(1/2)
     
     #model04_prediction
-    lars.fit(X_scaled_train[model_vars], y_train[target])
+    lars2.fit(X_train[model_features], y_train[target])
    
-    y_train['model04_pred'] = lars.predict(X_scaled_train[model_vars])
-    y_validate['model04_pred'] = lars.predict(X_scaled_validate[model_vars])
+    y_train['model04_pred'] = lars2.predict(X_train[model_features])
+    y_validate['model04_pred'] = lars2.predict(X_validate[model_features])
    
     rsme_model04_train = (mean_squared_error(y_train.logerror, y_train['model04_pred']))**(1/2)
     rsme_model04_validate = (mean_squared_error(y_validate.logerror, y_validate['model04_pred']))**(1/2)
+   
+    return pd.DataFrame({'OLS with Clusters':[rsme_model01_train, rsme_model01_validate, (rsme_model01_train-rsme_model01_validate)],
+                     'OLS without Clusters':[rsme_model03_train, rsme_model03_validate, (rsme_model03_train-rsme_model03_validate)],
+                     'LassoLars with Clusters':[rsme_model02_train, rsme_model02_validate, (rsme_model02_train-rsme_model02_validate)],
+                     'LassoLars without Clusters':[rsme_model04_train, rsme_model04_validate, (rsme_model04_train-rsme_model04_validate)],
+                     'Baseline':[rmse_baseline_train, rmse_baseline_validate, (rmse_baseline_train-rmse_baseline_validate)]},
+                    index=['train', 'validate', 'difference'])
+
+def model_features_df_report(X_scaled_train, X_scaled_validate, y_train, y_validate):
+#baseline_prediction
+    median = y_train.logerror.median()
+    y_train['baseline_pred'] = median
+    y_validate['baseline_pred'] = median
+   
+    rmse_baseline_train = mean_squared_error(y_train.logerror, y_train.baseline_pred)**(0.5)
+    rmse_baseline_validate = mean_squared_error(y_validate.logerror, y_validate.baseline_pred)**(0.5)
+
+   #model01_prediction
+    lm = LinearRegression(normalize=True)
+    lm.fit(X_scaled_train, y_train.logerror)
+   
+    y_train['model01_pred'] = lm.predict(X_scaled_train)
+    y_validate['model01_pred'] = lm.predict(X_scaled_validate)
+   
+    rsme_model01_train = (mean_squared_error(y_train.logerror, y_train['model01_pred']))**(0.5)
+    rsme_model01_validate = (mean_squared_error(y_validate.logerror, y_validate['model01_pred']))**(0.5)
+   
+    #model02_prediction
+    lars = LassoLars(alpha=1.0)
+    lars.fit(X_scaled_train, y_train.logerror)
+   
+    y_train['model02_pred'] = lars.predict(X_scaled_train)
+    y_validate['model02_pred'] = lars.predict(X_scaled_validate)
+   
+    rsme_model02_train = (mean_squared_error(y_train.logerror, y_train['model02_pred']))**(0.5)
+    rsme_model02_validate = (mean_squared_error(y_validate.logerror, y_validate['model02_pred']))**(0.5)
+
+    
+    #model03_prediction
+    lm = LinearRegression(normalize=True)
+    lm.fit(X_scaled_train[['age', 'sq_ft', 'tax_value']], y_train.logerror)
+   
+    y_train['model03_pred'] = lm.predict(X_scaled_train[['age', 'sq_ft', 'tax_value']])
+    y_validate['model03_pred'] = lm.predict(X_scaled_validate[['age', 'sq_ft', 'tax_value']])
+   
+    rsme_model03_train = (mean_squared_error(y_train.logerror, y_train['model03_pred']))**(0.5)
+    rsme_model03_validate = (mean_squared_error(y_validate.logerror, y_validate['model03_pred']))**(0.5)
+    
+    #model04_prediction
+    lars.fit(X_scaled_train[['age', 'sq_ft', 'tax_value']], y_train.logerror)
+   
+    y_train['model04_pred'] = lars.predict(X_scaled_train[['age', 'sq_ft', 'tax_value']])
+    y_validate['model04_pred'] = lars.predict(X_scaled_validate[['age', 'sq_ft', 'tax_value']])
+   
+    rsme_model04_train = (mean_squared_error(y_train.logerror, y_train['model04_pred']))**(0.5)
+    rsme_model04_validate = (mean_squared_error(y_validate.logerror, y_validate['model04_pred']))**(0.5)
    
     return pd.DataFrame({'OLS with Clusters':[rsme_model01_train, rsme_model01_validate, (rsme_model01_train-rsme_model01_validate)],
                      'OLS without Clusters':[rsme_model03_train, rsme_model03_validate, (rsme_model03_train-rsme_model03_validate)],
